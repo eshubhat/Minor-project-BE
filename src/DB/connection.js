02af4ef267eaaa363f153d41e9b00d4f124161f2
Eshan A. Bhat <eshubhat03@gmail.com>
2025-05-06 10:32:08 +0530

    process.env setup fixed

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const DBconnection = () => {
   mongoose.connect(
-    `mongodb+srv://kartikbevoor2130:M2130k2003@cluster0.vlbnl1e.mongodb.net/minorProject`,
+    `${process.env.MONGO_URI}`,
     console.log("DB connected")
   );
 };
